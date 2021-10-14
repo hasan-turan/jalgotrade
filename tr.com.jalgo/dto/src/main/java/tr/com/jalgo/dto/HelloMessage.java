529cc9418be30f148231ed08187cd80a01841379
Hasan Turan <hturan24@gmail.com>
2021-10-14 16:49:28 +0300

    testing web socket  getting json parse error

---
+++ tr.com.jalgo/dto/src/main/java/tr/com/jalgo/dto/HelloMessage.java
@@ -0,0 +1,26 @@
+package tr.com.jalgo.dto;
+
+import java.io.Serializable;
+
+public class HelloMessage implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+
+	public HelloMessage() {
+	}
+
+	public HelloMessage(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
